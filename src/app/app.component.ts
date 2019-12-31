1ec7a2537ae39c602d08aa6cf293aa2894f579c9
Tauhidul Alam <tauhid@Tauhiduls-MacBook-Pro.local>
2019-12-31 12:55:46 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { Component, NgZone } from '@angular/core';
+
+declare const annyang: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	voiceActiveSectionDisabled: boolean = true;
+	voiceActiveSectionError: boolean = false;
+	voiceActiveSectionSuccess: boolean = false;
+	voiceActiveSectionListening: boolean = false;
+	voiceText: any;
+
+  constructor(private ngZone: NgZone){}
+
+	initializeVoiceRecognitionCallback(): void {
+		annyang.addCallback('error', (err) => {
+			if (this.voiceText === undefined) {
+				this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+		});
+
+		annyang.addCallback('soundstart', (res) => {
+      this.ngZone.run(() => this.voiceActiveSectionListening = true);
+		});
+
+		annyang.addCallback('end', () => {
+      if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+		});
+
+		annyang.addCallback('result', (userSaid) => {
+			this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+			let queryText: any = userSaid[0];
+
+			annyang.abort();
+
+      this.voiceText = queryText;
+
+			this.ngZone.run(() => this.voiceActiveSectionListening = false);
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+		});
+	}
+
+	startVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = false;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+    this.voiceText = undefined;
+
+		this.initializeVoiceRecognitionCallback();
+
+		if (annyang) {
+			let commands = {
+				'demo-annyang': () => { }
+			};
+
+			annyang.addCommands(commands);
+
+			annyang.start({ autoRestart: false });
+		}
+	}
+
+	closeVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = true;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+		this.voiceActiveSectionListening = false;
+		this.voiceText = undefined;
+
+		if(annyang){
+      annyang.abort();
+    }
+	}
+}
